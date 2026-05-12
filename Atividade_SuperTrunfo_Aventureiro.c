68bb722bf7c1c2a66685dbf8c19878d1d2c7f27f
Kaian Mateus Oliveira dos Santos <kaianstsz@gmail.com>
2026-05-12 23:43:02 +0000

    Atividade SuperTrunfo Aventureiro completo

--- Atividade_SuperTrunfo_Aventureiro.c
+++ Atividade_SuperTrunfo_Aventureiro.c
@@ -11,10 +11,10 @@
                 int populacao2;
                 int npt1; // Numero de pontos turisticos
                 int npt2;
-                double area1;
-                double area2;
-                double pib1;
-                double pib2;
+                float area1;
+                float area2;
+                float pib1;
+                float pib2;
                 float dp1, dp2, pibc1, pibc2; //Densidade Populacional, PIB per Capita
 
                     printf("CARTA 01\n");
@@ -27,11 +27,11 @@
                     printf("População: \n");
                     scanf("%d", &populacao1);
                     printf("Area: \n");
-                    scanf("%lf", &area1);
+                    scanf("%f", &area1);
                     printf("Número de Pontos Turísticos: \n");
                     scanf("%d", &npt1);
                     printf("Digite o PIB: \n");
-                    scanf("%lf", &pib1);
+                    scanf("%f", &pib1);
                     
                     printf("CARTA 02\n");
                     printf("Estado: \n");
@@ -43,16 +43,16 @@
                     printf("População: \n");
                     scanf("%d", &populacao2);
                     printf("Área: \n");
-                    scanf("%lf", &area2);
+                    scanf("%f", &area2);
                     printf("PIB: \n");
-                    scanf("%lf", &pib2);
+                    scanf("%f", &pib2);
                     printf("Número de Pontos Turísticos: \n");
                     scanf("%d", &npt2);
 
                     dp1 = (float)(populacao1 / area1);
                     dp2 = (float)(populacao2 / area2);
-                    pibc1 = pib1 / populacao1;
-                    pibc2 = pib2 / populacao2;
+                    pibc1 = (pib1 * 1000000000) / populacao1;
+                    pibc2 = (pib2 * 1000000000) / populacao2;
 
                     printf("----------------------------------\n");
 
@@ -61,8 +61,8 @@
                         printf("Codigo: %s\n", cdc1);
                         printf("Nome da Cidade: %s\n", nc1);
                         printf("População: %d\n", populacao1);
-                        printf("Área: %lf km²\n", area1);
-                        printf("PIB: %lf bilhões de Reais\n", pib1);
+                        printf("Área: %f km²\n", area1);
+                        printf("PIB: %f bilhões de Reais\n", pib1);
                         printf("Número de Pontos Turísticos: %d\n", npt1);
                         printf("Densidade Populacional: %.2f Hab/km²\n", dp1);
                         printf("PIB per Capita: %.2f Reais\n", pibc1);
@@ -72,8 +72,8 @@
                         printf("Codigo: %s\n", cdc2);
                         printf("Nome da Cidade: %s\n", nc2);
                         printf("População: %d\n", populacao2);
-                        printf("Área: %lf km²\n", area2);
-                        printf("PIB: %lf bilhões de Reais\n", pib2);
+                        printf("Área: %f km²\n", area2);
+                        printf("PIB: %f bilhões de Reais\n", pib2);
                         printf("Número de Pontos Turísticos: %d\n", npt2);
                         printf("Densidade Populacional: %.2f Hab/km²\n", dp2);
                         printf("PIB per Capita: %.2f Reais\n", pibc2);
